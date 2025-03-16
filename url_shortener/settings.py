f375f889e90c9cf47f536e9a2a63dff75ebc8dfe
muqeetqazi <muqeetahmad202@gmail.com>
2025-03-16 07:18:14 +0500

    Make and apply migrations for ShortURL

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shortener'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'url_shortener',
+        'USER': 'root',
+        'PASSWORD': 'Qazi@12321',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
